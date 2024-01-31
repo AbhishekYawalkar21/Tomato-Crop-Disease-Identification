2fb1b03a8f9e78dba4808cbeb1fea527e27d0c20
AbhishekYawalkar21 <abhishekyawalkar4@gmail.com>
2024-01-30 20:21:56 -0500

    Changed logo

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from "expo-splash-screen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from "@react-navigation/native"
+import { FONTS } from './constants/fonts'; 
+import { useCallback } from 'react';
+import { Login, Signup, Welcome } from './screens';
+import { Provider } from 'react-redux'
+import { store } from './store/store'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [fontsLoaded] = useFonts(FONTS);
+
+  const onLayoutRootView = useCallback(async ()=>{
+    if(fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  },[fontsLoaded])
+
+  if(!fontsLoaded) {
+    return null;
+  }
+
+  return ( 
+    <Provider store={store}>
+   <SafeAreaProvider onLayout={onLayoutRootView}>
+      <NavigationContainer>
+        <Stack.Navigator 
+          screenOptions={{ headerShown: false }}
+          initialRouteName='Welcome'>
+        <Stack.Screen name="Welcome" component={Welcome}/>
+        <Stack.Screen name="Login" component={Login}/>
+        <Stack.Screen name="Signup" component={Signup}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+  </SafeAreaProvider>
+  </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
